1fe10e979a94b4859dab13df66ad0e2a362ab06f
manan <manandharabhinna@gmail.com>
2021-05-04 15:31:00 +0545

    login window created and weather API connected

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from tkinter import *
 import sqlite3
 import requests
 root=Tk()
+bg_color='PeachPuff2'
+root.config(bg=bg_color)
 root.resizable(0,0)
-
+root.title('Daily Routine')
 
 def weather_details(location):
     city_name = location
@@ -15,18 +17,46 @@ def weather_details(location):
     humidity = main['humidity']
     weather_array= weather_json['weather']
     weather=(weather_array[0]['main'])
-    temperature = main['temp']
-    weather_list={'weather':weather,'humidity':humidity,'temperature':temperature}
+    temperature_kelvin = main['temp']
+    temperature_celcius = temperature_kelvin - 273.15
+    weather_list={'weather':weather,'humidity':humidity,'temperature':temperature_celcius}
     return weather_list
 
+def login():
+    login_frame=LabelFrame(text='Enter User Details',bg=bg_color)
+    login_frame.grid(row=0,column=0)
+
+    username_lbl=Label(login_frame,text = 'Enter Name',font=('Helvetica',10),bg=bg_color,anchor=W)
+    username_lbl.grid(row=0,column=0)
+    location_lbl=Label(login_frame,text = 'Enter Location',font=('Helvetica',10),bg=bg_color,anchor=W)
+    location_lbl.grid(row=1,column=0,padx=4,pady=4)
+
+    location_entry=Entry(login_frame,width=30)
+    username_entry=Entry(login_frame,width=30)
+    username_entry.grid(row=0,column=1,padx=4,pady=4,columnspan=3)
+    location_entry.grid(row=1,column=1,padx=4,pady=4,columnspan=3)
+
+    press_button=Button(text='Login',width=20,bg='PeachPuff3', command=new_window)
+    press_button.grid(row=2,column=0,columnspan=2)
+def new_window():
+    global root
+    root.destroy()
+    info_win=Tk()
+    weather_label(info_win)
+    info_win.mainloop()
+def weather_label(surface):
+    outer_label=LabelFrame(surface,text="Current Weather",font=('Helvetica',8),bg='PeachPuff2',fg='Black')
+    weather_label = Label(outer_label,text=f"Weather: {weather_details('Kathmandu')['weather']}",font=('Helvetica',12),anchor=W,bg=bg_color)
+    humidity_label = Label(outer_label,text=f"Humidity: {weather_details('Kathmandu')['humidity']}",font=('Helvetica',12),anchor=W,bg=bg_color)
+    temp_Label = Label(outer_label,text= f"Temperature: {weather_details('Kathmandu')['temperature']}°C",font=('Helvetica',12),anchor=W,bg=bg_color)
+
+    outer_label.grid(row=0,column=0)
+    weather_label.grid(row=1,column=0,padx=2,pady=2)
+    temp_Label.grid(row=2,column=0,padx=2,pady=2)
+    humidity_label.grid(row=3,column=0,padx=2,pady=2)
 
 
-weather_label = Label(text=f"Weather: {weather_details('Kathmandu')['weather']}",anchor=W)
-humidity_label = Label(text=f"Humidity: {weather_details('Kathmandu')['humidity']}",anchor=W)
-temp_Label = Label(text= f"Temperature: {weather_details('Kathmandu')['temperature']}",anchor=W)
 
-
-weather_label.grid(row=0,column=0,padx=2,pady=2)
-humidity_label.grid(row=1,column=0,padx=2,pady=2)
-temp_Label.grid(row=2,column=0,padx=2,pady=2)
+if __name__=='__main__':
+    login()
 root.mainloop()
--- recurs.py
+++
@@ -1,6 +0,0 @@
-def table(num):
-    if limit==1:
-        return 3
-    else:
-        return num* times(limit-1)
-print(table(3))
